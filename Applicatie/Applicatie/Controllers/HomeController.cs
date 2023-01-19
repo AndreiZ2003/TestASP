029d7fff5ea19eba288b7dad4794cc2a3f95f0d0
AndreiZ2003 <123062755+AndreiZ2003@users.noreply.github.com>
2023-01-19 11:19:37 +0100

    Test

---
+++ Applicatie/Applicatie/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Applicatie.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace Applicatie.Controllers
+{
+    public class HomeController : Controller
+    {
+
+    }
+}
